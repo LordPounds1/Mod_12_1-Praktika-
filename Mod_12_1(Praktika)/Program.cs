749874e27eff8362c79ecca9ab1b759d4abbd030
LordPounds1 <91436904+LordPounds1@users.noreply.github.com>
2023-12-01 22:11:43 +0600

    Add project files.

---
+++ Mod_12_1(Praktika)/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+public class PropertyEventArgs : EventArgs
+{
+    public string PropertyName { get; set; }
+
+    public PropertyEventArgs(string propertyName)
+    {
+        PropertyName = propertyName;
+    }
+}
+
+public delegate void PropertyEventHandler(object sender, PropertyEventArgs e);
+
+public interface IPropertyChanged
+{
+    event PropertyEventHandler PropertyChanged;
+}
+
+public class MyClass : IPropertyChanged
+{
+    private int myProperty;
+
+    public int MyProperty
+    {
+        get { return myProperty; }
+        set
+        {
+            if (myProperty != value)
+            {
+                myProperty = value;
+                OnMyPropertyChanged(nameof(MyProperty));
+            }
+        }
+    }
+
+    public event PropertyEventHandler PropertyChanged;
+
+    protected virtual void OnMyPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyEventArgs(propertyName));
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        MyClass obj = new MyClass();
+        obj.PropertyChanged += (sender, e) =>
+        {
+            Console.WriteLine($"Property {e.PropertyName} changed");
+        };
+        obj.MyProperty = 42;
+
+        Console.ReadLine();
+    }
+}
+
